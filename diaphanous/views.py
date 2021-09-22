bbf74217e162a6c02e68d50dbbc25f53eeff4e7c
Ella Heron <heronsound95@gmail.com>
2021-09-22 09:29:57 -0400

    first commit

---
+++ diaphanous/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+
+from diaphanous import app
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/sound/")
+@app.route("/audio/")
+def sound():
+    return render_template('audio.html')
+
+@app.route("/hi")
+def three():
+    return render_template('hi.html')
+
+@app.errorhandler(404)
+def page_note_found(error):
+    return render_template('404.html'), 404
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='diaphanous',
+    packages=['diaphanous'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ]
+)
