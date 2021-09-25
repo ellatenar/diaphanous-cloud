6dabf531e8f13c0155774737405f6cd9ac4d2add
Ella Heron <heronsound95@gmail.com>
2021-09-24 23:36:42 -0400

    feat: add touch responsive infobox

--- diaphanous/static/script/main.js
+++ diaphanous/static/script/main.js
@@ -4,12 +4,20 @@ const footer = document.querySelector('#footer')
 const siteInfo = document.querySelector('#siteInfo')
 const info = document.querySelectorAll('.hidden')
 
-footer.addEventListener('mouseenter', () => {
+const showInfo = () => () => {
     siteInfo.style.visibility = 'visible'
     info.forEach(p => p.style.visibility = 'visible')
-})
+}
 
-footer.addEventListener('mouseleave', () => {
+const hideInfo = () => () => {
     siteInfo.style.visibility = 'hidden'
     info.forEach(p => p.style.visibility = 'hidden')
-})
+}
+
+footer.addEventListener('mouseenter', showInfo)
+footer.addEventListener('mouseleave', hideInfo)
+
+// Touch-responsive listeners for mobile
+footer.addEventListener('touchstart', showInfo)
+footer.addEventListener('touchend', hideInfo)
+footer.addEventListener('touchcancel', hideInfo)
