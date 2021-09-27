1d81300f8dc8307a031c120f1de73558f3ddd8b4
Ella Heron <heronsound95@gmail.com>
2021-09-27 17:12:29 -0400

    reverts bad commit :(

--- diaphanous/static/script/main.js
+++ diaphanous/static/script/main.js
@@ -1,23 +1,12 @@
-
-// Add event listener for site info popup
-const footer = document.querySelector('#footer')
 const siteInfo = document.querySelector('#siteInfo')
 const info = document.querySelectorAll('.hidden')
 
-const showInfo = () => () => {
+footer.addEventListener('mouseenter', () => {
     siteInfo.style.visibility = 'visible'
     info.forEach(p => p.style.visibility = 'visible')
-}
+})
 
-const hideInfo = () => () => {
+footer.addEventListener('mouseleave', () => {
     siteInfo.style.visibility = 'hidden'
     info.forEach(p => p.style.visibility = 'hidden')
-}
-
-footer.addEventListener('mouseenter', showInfo)
-footer.addEventListener('mouseleave', hideInfo)
-
-// Touch-responsive listeners for mobile
-footer.addEventListener('touchstart', showInfo)
-footer.addEventListener('touchend', hideInfo)
-footer.addEventListener('touchcancel', hideInfo)
+})
